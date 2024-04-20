7333fc73cabdbfe3fa3f66e7e9d7058a1ef743ee
Byeon-MJ <byunmj24@gmail.com>
2024-04-20 16:43:41 +0900

    IntoC Initial Commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#pragma warning(disable:4996) // 권장하지 않는 함수 사용에 대한 경고 메시지 무시(scanf 함수)
+extern void main_1();
+extern void array_ex();
+
+int main() {
+    array_ex();
+
+
+    return 0;
+}
